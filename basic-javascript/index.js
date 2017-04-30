e90db13f441c4b238af390ccd68c6e57ae727a51
Tu Nguyen <nvminhtu@gmail.com>
2017-04-30 22:06:07 +0700

    update basic javascript coding

---
+++ basic-javascript/index.js
@@ -0,0 +1,41 @@
+// demo link: http://2ality.com/2013/06/basic-javascript.html#sect_nature
+console.log(3+4); //7
+
+var foo; // declare variables
+
+var y =10;
+var x;
+if (y >= 0) {
+    x = y; 
+} else {
+    x = -y;
+}
+
+// expression
+var x = y >= 0 ? y: -y;
+
+// customFunction( y >=0? y: -y);
+
+// Semicolons
+var x = 3 * 7;
+var f = function() {};
+
+// Compund assignment Operators
+x += 1; 
+x = x + 1;
+
+
+// return typeof
+console.log(typeof true);
+console.log(typeof 'hello cu');
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof 123);
+console.log(typeof function() {});
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
+
+
